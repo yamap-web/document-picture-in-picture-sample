916ee7dc50ccf69c154c04afad8f4d09932d3fa1
yamap <yamap.web@gmail.com>
2025-02-11 13:45:17 +0900

    🎉

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const pipBtn = document.getElementById('pip-btn');
+
+pipBtn.addEventListener('click', async () => {
+  if (!(documentPictureInPicture in window)) {
+    alert('ピクチャーインピクチャーはサポートされていません');
+    return;
+  }
+
+  const pipContent = document.getElementById('pip-content');
+  const pipWindow = await window.documentPictureInPicture.requestWindow({
+    width: pipContent.clientWidth,
+    height: pipContent.clientHeight,
+  });
+
+  const pipBackground = window.getComputedStyle(pipContent).backgroundColor;
+  pipWindow.document.body.style.backgroundColor = pipBackground;
+
+  pipWindow.document.body.append(pipContent);
+
+  pipWindow.addEventListener('unload', (e) => {
+    const pipContainer = document.getElementById('pip-container');
+    const content = e.target.getElementById('pip-content');
+    pipContainer.append(content);
+  });
+});
